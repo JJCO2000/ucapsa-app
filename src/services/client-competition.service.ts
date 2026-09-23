8c1ccd404aa7e9ffd938ae199fd1132e5cdeede6
JJCO2000 <javi.cab.8a@gmail.com>
2026-09-22 18:28:53 -0600

    feat(competition): export account-level competition service

--- src/services/client-competition.service.ts
+++ src/services/client-competition.service.ts
@@ -1,5 +1,6 @@
 export * from './client-competition.types';
 export * from './client-competition-summary.service';
+export * from './client-competition-account.service';
 export * from './client-competition-exam.service';
 export * from './client-competition-constancy.service';
 export * from './client-competition-ranking.service';
